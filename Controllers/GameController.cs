c65e5d183f60f18cf9e2582d8b98f37bc2fbe58b
Kayateia <github@kayateia.net>
2010-10-16 10:24:52 -0700

    Implement thread-friendly push/pop for console output, and make game controller use it

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -10,23 +10,26 @@ using Kayateia.Climoo.Models;
 /// Controls the main game screen / loop.
 /// </summary>
 public class GameController : Controller {
+	// The actual main page view.
 	public ActionResult Index() {
-		// Initialize the user's session if it's not there already.
-		if (Session[UserState.SessionID] == null)
-			Session[UserState.SessionID] = new UserState();
-
 		return View("Console");
 	}
 
+	// Called periodically from the page for long-poll "push" notifications
+	// of console output.
 	public JsonResult PushCheck() {
+		IEnumerable<string> newLines = _state.outputPopAll();
+
 		var result = new ConsoleCommand() {
-			resultText = "push."
+			resultText = string.Join("<br/>", newLines)
 		};
 
 		System.Threading.Thread.Sleep(500);
 		return Json(result, JsonRequestBehavior.AllowGet);
 	}
 
+	// Called by the page when the user types a command. This may return
+	// data immediately rather than waiting for the push.
 	public JsonResult ExecCommand(string cmd) {
 		var result = new Models.ConsoleCommand() {
 			resultText = "Hey, you typed: " + cmd + "!"
@@ -34,6 +37,20 @@ public class GameController : Controller {
 
 		return Json(result, JsonRequestBehavior.AllowGet);
 	}
+
+	// Retrieve the user's state, or make a new one.
+	private UserState _state {
+		get {
+			if (_stateCache == null) {
+				// Initialize the user's session if it's not there already.
+				if (Session[UserState.SessionID] == null)
+					Session[UserState.SessionID] = new UserState();
+				_stateCache = (UserState)Session[UserState.SessionID];
+			}
+			return _stateCache;
+		}
+	}
+	private UserState _stateCache = null;
 }
 
 }
--- Models/UserState.cs
+++ Models/UserState.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 
 /// <summary>
@@ -14,7 +15,47 @@ public class UserState {
 	public UserState() {
 	}
 
-	public string pendingOutput = "";
+	/// <summary>
+	/// Adds a single line of output to the stack.
+	/// </summary>
+	public void outputPush(string line) {
+		outputPush(new string[] { line });
+	}
+
+	/// <summary>
+	/// Adds one or more lines of output to the stack.
+	/// </summary>
+	public void outputPush(IEnumerable<string> lines) {
+		lock (_mutex) {
+			_pendingOutput.AddRange(lines);
+			_pendingOutputEvent.Set();
+		}
+	}
+
+	/// <summary>
+	/// Removes all lines of output from the stack, and returns them.
+	/// </summary>
+	public IEnumerable<string> outputPopAll() {
+		lock (_mutex) {
+			if (_pendingOutput.Count == 0)
+				return new string[0];
+			var rv = _pendingOutput;
+			_pendingOutput = new List<string>();
+			_pendingOutputEvent.Reset();
+			return rv;
+		}
+	}
+
+	// Mutex which controls everything inside the state. This should always be
+	// locked before using things in here.
+	object _mutex = new object();
+
+	// A list of strings representing pending console output lines.
+	List<string> _pendingOutput = new List<string>();
+
+	// This event is set when there is output waiting to be sent, and reset
+	// when there is no output waiting.
+	ManualResetEvent _pendingOutputEvent = new ManualResetEvent(false);
 }
 
 }
