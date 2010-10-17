f013e3ad9fc264ca5402611d8156a28d17db064f
Kayateia <github@kayateia.net>
2010-10-17 11:10:10 -0700

    Reverse non-working async controller methods

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -5,12 +5,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Threading;
+using Trace = System.Diagnostics.Trace;
 using Kayateia.Climoo.Models;
 
 /// <summary>
 /// Controls the main game screen / loop.
 /// </summary>
-public class GameController : AsyncController {
+public class GameController : Controller {
 	// The actual main page view.
 	public ActionResult Index() {
 		return View("Console");
@@ -19,31 +20,19 @@ public class GameController : AsyncController {
 	// Called periodically from the page for long-poll "push" notifications
 	// of console output. It's sort of silly that we have to do this async
 	// pattern for this, but it's not terrible.
-	public void PushCheckAsync() {
-		AsyncManager.OutstandingOperations.Increment();
-
+	public JsonResult PushCheck() {
 		// Pull the piece of the session we care about.
 		UserState state = _state;
 
-		// Execute this in the background.
-		ThreadPool.QueueUserWorkItem((o) => {
-			// Wait for new output, and fail if we don't get any by 25 seconds.
-			IEnumerable<string> rv;
-			if (!state.outputWait(25000))
-				rv = new string[0];
-			else {
-				// Get what's there, if anything is left.
-				rv = state.outputPopAll();
-			}
+		// Wait for new output, and fail if we don't get any by 25 seconds.
+		IEnumerable<string> newLines;
+		if (!state.outputWait(25000))
+			newLines = new string[0];
+		else {
+			// Get what's there, if anything is left.
+			newLines = state.outputPopAll();
+		}
 
-			// Sync back up with ASP.
-			AsyncManager.Sync(() => {
-				AsyncManager.Parameters["newLines"] = rv;
-				AsyncManager.OutstandingOperations.Decrement();
-			});
-		});
-	}
-	public JsonResult PushCheckCompleted(IEnumerable<string> newLines) {
 		var result = new ConsoleCommand() {
 			resultText = string.Join("<br/>", newLines)
 		};
@@ -54,6 +43,7 @@ public class GameController : AsyncController {
 	// Called by the page when the user types a command. This may return
 	// data immediately rather than waiting for the push.
 	public JsonResult ExecCommand(string cmd) {
+		Trace.WriteLine("Executing command");
 		var result = new Models.ConsoleCommand() {
 			resultText = string.Join("<br/>", Commands.CommandController.Execute(_state, cmd).ToArray())
 		};
