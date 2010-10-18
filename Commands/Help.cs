42bd3e89ea7bfec53c5f5feeca39658f0e466be3
Kayateia <github@kayateia.net>
2010-10-17 17:32:22 -0700

    Improve help listing to show command list

--- Commands/Help.cs
+++ Commands/Help.cs
@@ -26,9 +26,13 @@ public class Help : ICommand {
 	public IEnumerable<string> execute(Session.UserContext userState, string input, string[] parameters) {
 		// Find the requested command, defaulting to us.
 		ICommand cmd;
-		if (parameters.Length == 1)
-			cmd = this;
-		else
+		if (parameters.Length == 1) {
+			List<string> outp = new List<string>();
+			outp.Add("The following commands are available:");
+			foreach (var c in CommandController.GetAll())
+				outp.Add(string.Format("{0} - {1}", c.command, c.name));
+			return outp;
+		} else
 			cmd = CommandController.LookUp(parameters[1]);
 
 		// Print out the help text.
