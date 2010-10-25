a22a619e9b49e5ca10b15732136c481ccc0de531
Kayateia <github@kayateia.net>
2010-10-24 21:15:34 -0700

    Default URL to /Game/Index for now

--- Global.asax.cs
+++ Global.asax.cs
@@ -16,9 +16,8 @@ public class MvcApplication : System.Web.HttpApplication {
 		routes.MapRoute(
 			"Default", // Route name
 			"{controller}/{action}/{id}", // URL with parameters
-			new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+			new { controller = "Game", action = "Index", id = UrlParameter.Optional } // Parameter defaults
 		);
-
 	}
 
 	protected void Application_Start() {
