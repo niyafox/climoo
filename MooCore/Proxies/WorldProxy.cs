ceccdce244b12020b00bd0139457ee33984e3ce1
Kayateia <github@kayateia.net>
2010-10-30 21:22:07 -0700

    Early start on a world proxy

---
+++ MooCore/Proxies/WorldProxy.cs
@@ -0,0 +1,60 @@
+﻿namespace Kayateia.Climoo.MooCore.Proxies {
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kayateia.Climoo.Scripting.SSharp;
+
+class WorldProxy : IDynamicObject {
+	public MobProxy get(int id) {
+		throw new NotImplementedException();
+	}
+
+	public MobProxy get(string path) {
+		throw new NotImplementedException();
+	}
+
+	static string[] s_ptmem = new string[0];
+	public bool isMemberPassthrough(string name) {
+		return (from i in s_ptmem where name.Equals(i, StringComparison.OrdinalIgnoreCase) select 1).Any();
+	}
+
+	public object getMember(string name) {
+		throw new NotImplementedException();
+	}
+
+	public string getMimeType(string name) {
+		throw new NotImplementedException();
+	}
+
+	public bool hasMember(string name) {
+		throw new NotImplementedException();
+	}
+
+	public IEnumerable<string> getMemberNames() {
+		throw new NotImplementedException();
+	}
+
+	public void setMember(string name, object val) {
+		throw new NotImplementedException();
+	}
+
+	public void setMimeType(string name, string type) {
+		throw new NotImplementedException();
+	}
+
+	public bool hasMethod(string name) {
+		throw new NotImplementedException();
+	}
+
+	static string[] s_ptmeth = new string[0];
+	public bool isMethodPassthrough(string name) {
+		return (from i in s_ptmeth where name.Equals(i, StringComparison.OrdinalIgnoreCase) select 1).Any();
+	}
+
+	public object callMethod(Scope scope, string name, object[] args) {
+		throw new NotImplementedException();
+	}
+}
+
+}
