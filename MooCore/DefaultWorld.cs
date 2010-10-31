4addf038e1ebb19fa46cd6daa5957c1945728d0a
Kayateia <github@kayateia.net>
2010-10-30 19:24:09 -0700

    Kind of ugly defaultworld hack to get separately-called look verbs, and give the conduit her pretty picture

--- MooCore/DefaultWorld.cs
+++ MooCore/DefaultWorld.cs
@@ -7,41 +7,10 @@ using System.Text;
 // Define a default world if desired. This is kept away from the
 // rest to keep the code cleaner.
 public partial class World {
-	static public World CreateDefault() {
-		World w = new World();
-
-		// This object will be the spiritual parent of every object in the MOO. Its
-		// ID should always be #1.
-		Mob ptb = w.createObject(new {
-			name = "The Powers That Be",
-			desc = "The 'god' object: It's all downhill from here, baby."
-		}, parent: Mob.None.id);
-		System.Diagnostics.Debug.Assert(ptb.id == 1);
-
-		Mob templates = w.createObject(new {
-			name = "Template Room",
-			desc = "Everything that's a base object will go in here for easy findin's.",
-			pathid = "templates"
-		}, location: ptb.id);
-
-		Mob playerTemplate = w.createObject(new {
-			name = "Player",
-			desc = "A confused looking person wanders around with a 'help me' sticky note on their back.",
-			pathid = "player"
-		}, location: templates.id);
-
-		Mob roomTemplate = w.createObject(new {
-			name = "Room",
-			desc = "A simple room, so simple really that it has no description.",
-			pathid = "room"
-		}, location: templates.id);
-		roomTemplate.verbs["look"] = new Verb() {
-			name = "look",
-			help = "Look at the room",
-			code = @"
-				//verb
-				//verb self
-				//verb around
+	static string getLook(string contentsLabel) {
+		return @"
+				if (prep == ""at"")
+					self = indobj;
 				sb = new StringBuilder();
 				sb.AppendFormat(""[b]{0}[/b] ({1})"",
 					self.name,
@@ -59,7 +28,7 @@ public partial class World {
 						}
 					}
 					if (hasNonPlayer) {
-						sb.Append(""\n[b]Also here:[/b] "");
+						sb.Append(""\n[b]##CONTENTS##[/b] "");
 						foreach (obj in contents) {
 							if (obj.id != player.id)
 								sb.AppendFormat(""{0} ,"", obj.name);
@@ -69,7 +38,61 @@ public partial class World {
 					}
 				}
 				player.write(sb.ToString());
-			"
+			".Replace("##CONTENTS##", contentsLabel);
+	}
+	static public World CreateDefault() {
+		World w = new World();
+
+		// This object will be the spiritual parent of every object in the MOO. Its
+		// ID should always be #1.
+		Mob ptb = w.createObject(new {
+			name = "The Powers That Be",
+			desc = "The 'god' object: It's all downhill from here, baby."
+		}, parent: Mob.None.id);
+		System.Diagnostics.Debug.Assert(ptb.id == 1);
+		ptb.verbs["look"] = new Verb() {
+			name = "look",
+			help = "Look at something",
+			code = @"
+				//verb
+				//verb none around
+				//verb none at self
+				" + getLook("Also here")
+		};
+
+		Mob templates = w.createObject(new {
+			name = "Template Room",
+			desc = "Everything that's a base object will go in here for easy findin's.",
+			pathid = "templates"
+		}, location: ptb.id);
+
+		Mob playerTemplate = w.createObject(new {
+			name = "Player",
+			desc = "A confused looking person wanders around with a 'help me' sticky note on their back.",
+			pathid = "player"
+		}, location: templates.id);
+		playerTemplate.verbs["look"] = new Verb() {
+			name = "look",
+			help = "Look at someone",
+			code = @"
+				//verb none at self
+				//verb self
+				" + getLook("Has:")
+		};
+
+		Mob roomTemplate = w.createObject(new {
+			name = "Room",
+			desc = "A simple room, so simple really that it has no description.",
+			pathid = "room"
+		}, location: templates.id);
+		roomTemplate.verbs["look"] = new Verb() {
+			name = "look",
+			help = "Look at the room",
+			code = @"
+				//verb
+				//verb none around
+				//verb self
+				" + getLook("Also here: ")
 		};
 
 		Mob entryWay = w.createObject(new {
@@ -85,8 +108,8 @@ public partial class World {
 			name = "The Conduit",
 			desc = "A little girl in a red dress, or a panther with the coat of deepest black; it really "
 				+ "depends on who's looking.",
-			pathid = "conduit" /*,
-			image = TypedAttribute.LoadFromFile(@"d:\game\climoo\gamedata\conduit.jpg") */
+			pathid = "conduit",
+			image = TypedAttribute.LoadFromFile(@"d:\game\climoo\gamedata\conduit.jpg")
 		}, location: entryWay.id);
 
 		return w;
