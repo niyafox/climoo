248a67ee9f84134c2db1a157453fa22e350e3b77
Kayateia <github@kayateia.net>
2010-11-25 15:43:10 -0800

    Insert tabs in popup text editors at the cursor position, not the end.

--- Scripts/kayateia.climoo.editors.js
+++ Scripts/kayateia.climoo.editors.js
@@ -96,6 +96,9 @@ $(document).ready(function() {
 	ObjectEditor.init();
 });
 
+// Imported from http://www.scottklarr.com/topic/425/how-to-insert-text-into-a-textarea-where-the-cursor-is/
+function insertAtCaret(txtarea,text) { var scrollPos = txtarea.scrollTop; var strPos = 0; var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? "ff" : (document.selection ? "ie" : false ) ); if (br == "ie") { txtarea.focus(); var range = document.selection.createRange(); range.moveStart ('character', -txtarea.value.length); strPos = range.text.length; } else if (br == "ff") strPos = txtarea.selectionStart; var front = (txtarea.value).substring(0,strPos); var back = (txtarea.value).substring(strPos,txtarea.value.length); txtarea.value=front+text+back; strPos = strPos + text.length; if (br == "ie") { txtarea.focus(); var range = document.selection.createRange(); range.moveStart ('character', -txtarea.value.length); range.moveStart ('character', strPos); range.moveEnd ('character', 0); range.select(); } else if (br == "ff") { txtarea.selectionStart = strPos; txtarea.selectionEnd = strPos; txtarea.focus(); } txtarea.scrollTop = scrollPos; } 
+
 TextEditor = {
 	_popup: null,
 	_callback: null,
@@ -124,7 +127,8 @@ TextEditor = {
 
 		$('#texteditor textarea').bind('keydown', function(evt) {
 			if (evt.keyCode == 9) {
-				$('#texteditor textarea').val($('#texteditor textarea').val() + "\t");
+				var txted = $('#texteditor textarea')[0];
+				insertAtCaret(txted, "\t");
 				return false;
 			}
 		});
