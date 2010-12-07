744ba779f0d28598e456a7287117856c6c6b309d
Kayateia <github@kayateia.net>
2010-12-06 22:36:34 -0800

    Add help and about to "lobby" task, and make it simpler to add new commands

--- Tasks/PublicSite.cs
+++ Tasks/PublicSite.cs
@@ -6,7 +6,14 @@ using System.Web;
 
 public class PublicSite : UITask {
 	public PublicSite(Session.UserContext context) : base(context) {
+		_commands = new Dictionary<StringI,Func<IEnumerable<Result>>> {
+			{ "login", this.login },
+			{ "help", () => { return showScreen("help"); } },
+			{ "about", () => { return showScreen("about"); } }
+		};
 	}
+
+	Dictionary<StringI, Func<IEnumerable<Result>>> _commands;
 
 	protected override IEnumerable<Result> runTask() {
 		showWelcome();
@@ -14,44 +21,57 @@ public class PublicSite : UITask {
 		for ( ; ; ) {
 			yield return Result.GetInput();
 
-			if (_input.Length == 0) {
+			Func<IEnumerable<Result>> handler = null;
+			string[] pieces = _input.Split(' ', '\t');
+			if (pieces.Length > 0)
+				handler = getCommand(pieces[0]);
+
+			if (handler == null) {
+				_context.outputPush("Sorry, don't know what that means.");
 				showWelcome();
 				continue;
 			}
 
-			if (!_input.StartsWithI("login")) {
-				_context.outputPush("Sorry, only 'login' is allowed here.");
-				continue;
-			}
-
-			if (_input.EqualsI("login")) {
-				_context.outputPush("&gt;&gt; Login: ");
-				yield return Result.GetInput();
-				_input = "login " + _input;
-			}
-
-			string username = _input.Substring("login ".Length);
-
-			_context.outputPush("&gt;&gt;Password: ");
-			yield return Result.GetInput();
-			string password = _input;
-
-			string result = Game.Login.LogUserIn(_context, username, password);
-			if (result != null) {
-				_context.outputPush("Could not log you in: " + result);
-				continue;
-			}
-
-			_context.outputPush("\nSuddenly you're falling...!\n\n");
-			MooCore.InputParser.ProcessInput("look", _context.player);
-			yield return Result.ToGame();
+			foreach (var i in handler()) yield return i;
 		}
+	}
+
+	IEnumerable<Result> login() {
+		if (_input.EqualsI("login")) {
+			_context.outputPush("&gt;&gt; Login: ");
+			yield return Result.GetInput();
+			_input = "login " + _input;
+		}
+
+		string username = _input.Substring("login ".Length);
+
+		_context.outputPush("&gt;&gt;Password: ");
+		yield return Result.GetInput();
+		string password = _input;
+
+		string result = Game.Login.LogUserIn(_context, username, password);
+		if (result != null) {
+			_context.outputPush("Could not log you in: " + result);
+			yield break;
+		}
+
+		_context.outputPush("\nSuddenly you're falling...!\n\n");
+		MooCore.InputParser.ProcessInput("look", _context.player);
+		yield return Result.ToGame();
+	}
+
+	Func<IEnumerable<Result>> getCommand(string cmd) {
+		return (from c in _commands where c.Key == cmd select c.Value).FirstOrDefault();
+	}
+
+	IEnumerable<Result> showScreen(string screen) {
+		_context.outputPush(Models.Screens.Get(screen));
+		yield break;
 	}
 
 	void showWelcome() {
 		string welcome = Models.Screens.Get("welcome");
-		welcome += "\n"
-			+ "Commands: login";
+		welcome += "\nCommands: " + String.Join(" ", _commands.Keys);
 		_context.outputPush(welcome);
 	}
 }
