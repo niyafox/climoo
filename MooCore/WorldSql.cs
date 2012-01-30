64e8cccec489db74bb0658ddf014c47884fef09f
Kayateia <github@kayateia.net>
2012-01-29 22:44:51 -0800

    Get DB loading working

--- MooCore/WorldSql.cs
+++ MooCore/WorldSql.cs
@@ -148,107 +148,123 @@ public partial class World {
 	}
 
 	public bool loadFromSql() {
-		using (var context = new Sql.MooCoreSqlDataContext()) {
-			context.Connection.Open();
+		using (var context = new Sql.MooCoreSqlDataContext())
+			return loadFromContext(context);
+	}
 
-			// Find the latest checkpoint.
-			var cp = (from c in context.GetTable<Sql.Checkpoint>()
-						orderby c.time descending
-						select c).FirstOrDefault();
-			if (cp == null)
-				return false;
+	public bool loadFromSql(string connectionString) {
+		using (var context = new Sql.MooCoreSqlDataContext(connectionString))
+			return loadFromContext(context);
+	}
 
-			var mobtable = context.GetTable<Sql.Mob>();
-			var attrtable = context.GetTable<Sql.Attribute>();
-			var verbtable = context.GetTable<Sql.Verb>();
-			var worldtable = context.GetTable<Sql.World>();
+	public bool loadFromContext(System.Data.Linq.DataContext context) {
+		context.Connection.Open();
 
-			// Try to select out the next ID -- if we don't have this, give up.
-			int? nextid = (from row in worldtable
-						where row.name == "nextid" && row.checkpoint == cp.id
-						select row.intvalue).FirstOrDefault();
-			if (!nextid.HasValue)
-				return false;
-			_nextId = nextid.Value;
+		// Find the latest checkpoint.
+		var cp = (from c in context.GetTable<Sql.Checkpoint>()
+					orderby c.time descending
+					select c).FirstOrDefault();
+		if (cp == null)
+			return false;
 
-			// Alright, we're clear to proceed. Go ahead and query every thing
-			// out for fast loading, and we'll match up in memory.
-			//
-			// FIXME: LINQ doesn't support IN(foo). >_< So we have to just load
-			// every attribute and verb for every version of every object, ever,
-			// and sort through it in memory. Clearly this isn't tenable long term.
-			// Perhaps a join or manual SQL later.
-			var allmobs = from row in mobtable
-							where row.checkpoint == cp.id
-							select row;
-			var allattrs = from row in attrtable
-							select row;
-			var allverbs = from row in verbtable
-							select row;
+		var mobtable = context.GetTable<Sql.Mob>();
+		var attrtable = context.GetTable<Sql.Attribute>();
+		var verbtable = context.GetTable<Sql.Verb>();
+		var worldtable = context.GetTable<Sql.World>();
 
-			// Match up attributes with mobs.
-			var attrmatches = new Dictionary<int,List<Sql.Attribute>>();
-			foreach (var attr in allattrs) {
-				if (!attrmatches.ContainsKey(attr.@object))
-					attrmatches[attr.@object] = new List<Sql.Attribute>();
-				attrmatches[attr.@object].Add(attr);
-			}
+		// Try to select out the next ID -- if we don't have this, give up.
+		int? nextid = (from row in worldtable
+					where row.name == "nextid" && row.checkpoint == cp.id
+					select row.intvalue).FirstOrDefault();
+		if (!nextid.HasValue)
+			return false;
+		_nextId = nextid.Value;
 
-			// Match up verbs with mobs.
-			var verbmatches = new Dictionary<int,List<Sql.Verb>>();
-			foreach (var verb in allverbs) {
-				if (!verbmatches.ContainsKey(verb.@object))
-					verbmatches[verb.@object] = new List<Sql.Verb>();
-				verbmatches[verb.@object].Add(verb);
-			}
+		// Alright, we're clear to proceed. Go ahead and query every thing
+		// out for fast loading, and we'll match up in memory.
+		//
+		// FIXME: LINQ doesn't support IN(foo). >_< So we have to just load
+		// every attribute and verb for every version of every object, ever,
+		// and sort through it in memory. Clearly this isn't tenable long term.
+		// Perhaps a join or manual SQL later.
+		var allmobs = from row in mobtable
+						where row.checkpoint == cp.id
+						select row;
+		var allattrs = from row in attrtable
+						select row;
+		var allverbs = from row in verbtable
+						select row;
 
-			// Load up the mobs themselves.
-			foreach (var m in allmobs) {
-				var mob = new Mob(this, m.objectid) {
-					parentId = m.parent.HasValue ? m.parent.Value : 0,
-					locationId = m.location.HasValue ? m.location.Value : 0,
-					ownerId = m.owner,
-					perms = m.perms
-				};
-				if (m.pathid != null)
-					mob.pathId = m.pathid;
-
-				// Load up its attributes.
-				if (attrmatches.ContainsKey(m.id)) {
-					foreach (var attr in attrmatches[m.id]) {
-						TypedAttribute ta;
-						if (attr.textcontents != null)
-							ta = TypedAttribute.FromValue(attr.textcontents);
-						else
-							ta = TypedAttribute.FromPersisted(attr.datacontents.ToArray(), attr.mimetype);
-						ta.perms = attr.perms;
-						mob.attrSet(attr.name, ta);
-					}
-				}
-
-				// Load up its verbs.
-				if (verbmatches.ContainsKey(m.id)) {
-					foreach (var verb in verbmatches[m.id]) {
-						Verb v = new Verb() {
-							name = verb.name,
-							code = verb.code,
-							perms = verb.perms
-						};
-						mob.verbSet(verb.name, v);
-					}
-				}
-
-				_objects[mob.id] = mob;
-			}
-
-			// Everything loaded properly.
-			return true;
+		// Match up attributes with mobs.
+		var attrmatches = new Dictionary<int,List<Sql.Attribute>>();
+		foreach (var attr in allattrs) {
+			if (!attrmatches.ContainsKey(attr.@object))
+				attrmatches[attr.@object] = new List<Sql.Attribute>();
+			attrmatches[attr.@object].Add(attr);
 		}
+
+		// Match up verbs with mobs.
+		var verbmatches = new Dictionary<int,List<Sql.Verb>>();
+		foreach (var verb in allverbs) {
+			if (!verbmatches.ContainsKey(verb.@object))
+				verbmatches[verb.@object] = new List<Sql.Verb>();
+			verbmatches[verb.@object].Add(verb);
+		}
+
+		// Load up the mobs themselves.
+		foreach (var m in allmobs) {
+			var mob = new Mob(this, m.objectid) {
+				parentId = m.parent.HasValue ? m.parent.Value : 0,
+				locationId = m.location.HasValue ? m.location.Value : 0,
+				ownerId = m.owner,
+				perms = m.perms
+			};
+			if (m.pathid != null)
+				mob.pathId = m.pathid;
+
+			// Load up its attributes.
+			if (attrmatches.ContainsKey(m.id)) {
+				foreach (var attr in attrmatches[m.id]) {
+					TypedAttribute ta;
+					if (attr.textcontents != null)
+						ta = TypedAttribute.FromValue(attr.textcontents);
+					else
+						ta = TypedAttribute.FromPersisted(attr.datacontents.ToArray(), attr.mimetype);
+					ta.perms = attr.perms;
+					mob.attrSet(attr.name, ta);
+				}
+			}
+
+			// Load up its verbs.
+			if (verbmatches.ContainsKey(m.id)) {
+				foreach (var verb in verbmatches[m.id]) {
+					Verb v = new Verb() {
+						name = verb.name,
+						code = verb.code,
+						perms = verb.perms
+					};
+					mob.verbSet(verb.name, v);
+				}
+			}
+
+			_objects[mob.id] = mob;
+		}
+
+		// Everything loaded properly.
+		return true;
 	}
 
 	static public World FromSql() {
 		World w = new World();
 		if (!w.loadFromSql())
+			return null;
+		else
+			return w;
+	}
+
+	static public World FromSql(string connectionString) {
+		World w = new World();
+		if (!w.loadFromSql(connectionString))
 			return null;
 		else
 			return w;
