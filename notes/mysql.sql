40bba099eeea71bad0f6b9b648f2c1a1e3394c09
Kayateia <github@kayateia.net>
2014-07-07 00:05:10 -0700

    Add SQL update scripts for previous timer changes.

--- notes/mysql.sql
+++ notes/mysql.sql
@@ -1,3 +1,21 @@
+/*
+	CliMOO - Multi-User Dungeon, Object Oriented for the web
+	Copyright (C) 2010-2014 Kayateia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 DROP TABLE IF EXISTS `mob`;
 CREATE TABLE `mob` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -7,6 +25,7 @@ CREATE TABLE `mob` (
   `locationid` int DEFAULT NULL,
   `perms` int NOT NULL,
   `ownerid` int NOT NULL,
+  `pulse` tinyint NOT NULL,
   PRIMARY KEY (`id`)
 );
 
--- notes/postgres.sql
+++ notes/postgres.sql
@@ -1,3 +1,21 @@
+/*
+	CliMOO - Multi-User Dungeon, Object Oriented for the web
+	Copyright (C) 2010-2014 Kayateia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 create table mob (
 	id serial primary key,
 	objectid int not null,
@@ -5,7 +23,8 @@ create table mob (
 	pathid varchar(255) not null,
 	locationid int,
 	perms int not null,
-	ownerid int not null
+	ownerid int not null,
+	pulse tinyint not null
 );
 create table verb (
 	id serial primary key,
