01db1f3a359f861b10f8d0e3c90cba9b50625a61
Kayateia <github@kayateia.net>
2014-07-19 08:53:27 -0700

    Add JSON persistence utils

---
+++ Base/JsonPersistence.cs
@@ -0,0 +1,76 @@
+﻿/*
+	CliMOO - Multi-User Dungeon, Object Oriented for the web
+	Copyright (C) 2010-2014 Kayateia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Kayateia.Climoo
+{
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+
+	public class JsonPersistence
+{
+	/// <summary>
+	/// Converts a JSON string into an object.
+	/// </summary>
+	static public object Deserialize( Type t, string json )
+	{
+		byte[] bytes = System.Text.Encoding.UTF8.GetBytes( json );
+		var stream = new MemoryStream( bytes );
+		var serializer = new DataContractJsonSerializer( t );
+		return serializer.ReadObject( stream );
+	}
+
+	/// <summary>
+	/// Converts a JSON string into a typed object.
+	/// </summary>
+	static public T Deserialize<T>( string json )
+	{
+		return (T)Deserialize( typeof( T ), json );
+	}
+
+	/// <summary>
+	/// Converts an object into a JSON string.
+	/// </summary>
+	static public string Serialize( object o )
+	{
+		var serializer = new DataContractJsonSerializer( o.GetType() );
+		var stream = new MemoryStream();
+		serializer.WriteObject( stream, o );
+		byte[] output = stream.ToArray();
+		return System.Text.Encoding.UTF8.GetString( output );
+	}
+
+	/// <summary>
+	/// Returns the Type for a type short name, if it's been loaded into the AppDomain.
+	/// </summary>
+	/// <remarks>
+	/// This is only tangentially related, but it's used elsewhere in CliMOO in JSON serialization.
+	/// 
+	/// http://stackoverflow.com/a/7286354
+	/// </remarks>
+	static public Type GetTypeEx( string fullTypeName )
+	{
+		return Type.GetType( fullTypeName ) ??
+				AppDomain.CurrentDomain.GetAssemblies()
+						.Select( a => a.GetType( fullTypeName ) )
+						.FirstOrDefault( t => t != null );
+	}
+}
+
+}
---
+++ tests/ClimooTests/Serialization.cs
@@ -0,0 +1,73 @@
+﻿/*
+	CliMOO - Multi-User Dungeon, Object Oriented for the web
+	Copyright (C) 2010-2014 Kayateia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Kayateia.Climoo.Tests
+{
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+	using System.Runtime.Serialization;
+
+[TestFixture]
+public class Serialization
+{
+	[Test]
+	public void Json()
+	{
+		var tc = new TestClass()
+		{
+			foo = "foo",
+			bar = 5,
+			too = new TestClass2()
+			{
+				strings = new string[] { "foo", "bar", "baz" },
+				ints = new int[] { 1, 2, 3, 4, 5 }
+			}
+		};
+
+		string json = JsonPersistence.Serialize( tc );
+		var back = JsonPersistence.Deserialize<TestClass>( json );
+		Assert.AreEqual( tc.foo, back.foo );
+		Assert.AreEqual( tc.bar, back.bar );
+		Assert.AreEqual( tc.too.strings, back.too.strings );
+		Assert.AreEqual( tc.too.ints, back.too.ints );
+	}
+
+	[DataContract]
+	class TestClass
+	{
+		[DataMember]
+		public string foo;
+		[DataMember]
+		public int bar;
+		[DataMember]
+		public TestClass2 too;
+	}
+	[DataContract]
+	class TestClass2
+	{
+		[DataMember]
+		public string[] strings;
+		[DataMember]
+		public int[] ints;
+	}
+}
+
+}
